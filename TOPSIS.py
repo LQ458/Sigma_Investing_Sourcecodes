c1a1297b8da9a3fbefb2168e5adbc916ddcf7691
LQ458 <yihao.qin17311-biph@basischina.com>
2023-11-07 21:04:42 +0800

    Update TOPSIS.py

--- TOPSIS.py
+++ TOPSIS.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 
 # matrix
-# Country name following by 4 indices
 data = [
     ['Brazil', 8.948, 0.47, 1.78, 5.84],
     ['China', 11.7, 4.69, 4.92, 1.78],
@@ -12,42 +11,45 @@ data = [
     ['USA', 71.114, 0.01, 1.92, 3.98],
 ]
 
-# transfer to pandas DataFrame
 columns = ['Country', 'GDP per Capita', 'Current Account', 'rGDP growth', 'Inflation Rate']
 df = pd.DataFrame(data, columns=columns)
+print("Initial DataFrame:")
 print(df)
 
-# Data Normalization
 df.iloc[:, 1:] = df.iloc[:, 1:].apply(lambda x: x / np.sqrt((x**2).sum()))
-print('Normalized Data:')
+print("\nNormalized DataFrame:")
 print(df)
 
-# weight
 weights = [0.35, 0.05, 0.3, 0.3]
-
-# weight assigned to each index
 df.iloc[:, 1:] = df.iloc[:, 1:].multiply(weights, axis=1)
-print('Weighted Data:')
+
+print("\nWeighted DataFrame:")
 print(df)
 
-# calculating positive and negative ideal solutios
-# The first three indicators are considered better when they are larger (positive ideal), while the fourth indicator is considered better when it is smaller (negative ideal).
-positive_ideal_solution = df.iloc[:, 1:4].max(axis=0).tolist() + [df.iloc[:, 4].min()]
-negative_ideal_solution = df.iloc[:, 1:4].min(axis=0).tolist() + [df.iloc[:, 4].max()]
-print('Positive Ideal Solutions:', positive_ideal_solution)
-print('Negative Ideal Solutions:', negative_ideal_solution)
+positive_ideal_solution = pd.Series(
+    df.iloc[:, 1:4].max(axis=0).tolist() + [df.iloc[:, 4].min()],
+    index=df.columns[1:],
+)
+negative_ideal_solution = pd.Series(
+    df.iloc[:, 1:4].min(axis=0).tolist() + [df.iloc[:, 4].max()],
+    index=df.columns[1:],
+)
+
+print("\nPositive Ideal Solution:")
+print(positive_ideal_solution)
+print("\nNegative Ideal Solution:")
+print(negative_ideal_solution)
 
 # calculate the Euclidean distance of each country
 df['d_pos'] = np.sqrt(((df.iloc[:, 1:] - positive_ideal_solution) ** 2).sum(axis=1))
 df['d_neg'] = np.sqrt(((df.iloc[:, 1:] - negative_ideal_solution) ** 2).sum(axis=1))
-print('The Euclidean Distance of Each Country:')
+print("\nEuclidean Distance of Each Country:")
 print(df[['Country', 'd_pos', 'd_neg']])
 
-# calculate relative closeness
 df['closeness'] = df['d_neg'] / (df['d_pos'] + df['d_neg'])
-print('Relative Closeness:')
+print("\nRelative Closeness:")
 print(df[['Country', 'closeness']])
 
 # rank
-print('Ranked Countries:')
+print('\nRanked Countries:')
 print(df.sort_values(by='closeness', ascending=False)[['Country', 'closeness']])
