d089495c46064e259818a5d1d2a093487c5870fd
LQ <143152892+LQ458@users.noreply.github.com>
2023-11-30 12:20:04 +0800

    Update longRun.py

--- ETF_Selection/longRun.py
+++ ETF_Selection/longRun.py
@@ -24,61 +24,49 @@ class ETF:
         self.std_dev_1yr = std_dev_1yr
         self.actively_managed = actively_managed
 
-    def calculate_score(self):
-        # Use information entropy to structure the analysis
+    def calculate_aggressive_score(self):
         score = 0
 
-        # Calculate the score for asset class using information entropy
+        # Standard Deviation (30% weight)
+        std_dev_entropy = self.calculate_entropy(["Low", "Medium", "High"],
+                                                 [0.1, 0.3, 0.6], self.categorize_std_dev())
+        score += std_dev_entropy * 30
+
+        # Market Cap Focus (25% weight)
+        market_cap_entropy = self.calculate_entropy(["Small-cap", "Mid-cap", "Large-cap"],
+                                                    [0.4, 0.35, 0.25], self.market_cap_focus)
+        score += market_cap_entropy * 25
+
+        # Asset Class (25% weight)
         asset_class_entropy = self.calculate_entropy(["Equity", "Fixed Income"],
-                                                     [0.55, 0.45], self.asset_class)
-        score += asset_class_entropy * 10
+                                                     [0.7, 0.3], self.asset_class)
+        score += asset_class_entropy * 25
 
-        # Calculate the score for market focus using information entropy
-        market_cap_entropy = self.calculate_entropy(["Large-cap", "Mid-cap", "Small-cap", "Broad Market"],
-                                                    [0.25, 0.2, 0.15, 0.4], self.market_cap_focus)
-        score += market_cap_entropy * 10
-
-        # Calculate the score for investment strategy using information entropy
-        strategy_entropy = self.calculate_entropy(
-            ["Blend", "Growth", "Value", "Aggregate", "Corporate", "Government", "Municipals", "Mortgage-Backed", "Inflation Protected"],
-            [0.12, 0.12, 0.12, 0.12, 0.1, 0.1, 0.08, 0.08, 0.08],
-            self.strategy
-        )
-        score += strategy_entropy * 10
-
-        # Use of social realities to adjust scores for industry concerns
-        industry_score = self.adjust_industry_score(self.industry_focus)
-        score += industry_score * 10
-
-        # Using Information Entropy to Calculate Scores for Geographic Concerns
-        geographical_entropy = self.calculate_entropy(["United States",""],
-                                                      [0.7, 0.3], self.geographical_focus)
-        score += geographical_entropy * 10
-
-        # Using Information Entropy to Calculate Ratings of Concerns
-        rating_entropy = self.calculate_entropy(["Investment Grade A or higher", "Investment Grade BBB or higher", "High Yield"],
-                                                [0.35, 0.3, 0.35], self.rating_class_focus)
-        score += rating_entropy * 10
-
-        # Using information entropy to compute scores for maturity periods
-        maturity_band_entropy = self.calculate_entropy(["Short-Term", "Intermediate", "Ultra Short"],
-                                                       [0.3, 0.3, 0.4], self.maturity_band)
-        score += maturity_band_entropy * 10
-
-        # Use of expense ratio calculation scores
-        expense_ratio_weight = 1 - (self.expense_ratio / 0.3)
-        score += max(0, expense_ratio_weight) * 10
-
-        # Calculating scores using standard deviation
-        std_dev_weight = 1 - (self.std_dev_1yr / 30)
-        score += max(0, std_dev_weight) * 10
-
-        # Weight for active management
-        # Add 2 points if the ETF is actively managed
-        if self.actively_managed == "Y":
-            score += 2
+        # Actively Managed (20% weight)
+        actively_managed_weight = 1 if self.actively_managed == "Y" else 0
+        score += actively_managed_weight * 20
 
         return score
+    
+    # Helper methods for categorizing indicators
+    def categorize_expense_ratio(self):
+        # Categorize the expense ratio
+        if self.expense_ratio < 0.1:
+            return "Low"
+        elif self.expense_ratio < 0.2:
+            return "Medium"
+        else:
+            return "High"
+
+    def categorize_std_dev(self):
+        # Categorize the standard deviation
+        if self.std_dev_1yr < 10:
+            return "Low"
+        elif self.std_dev_1yr < 20:
+            return "Medium"
+        else:
+            return "High"
+
 
     @staticmethod
     def calculate_entropy(labels, weights, value):
@@ -90,27 +78,9 @@ class ETF:
         
         return entropy(prob_distribution)
 
-    @staticmethod
-    def adjust_industry_score(industry_focus):
-        # Adjusting the scoring of industry concerns to the actual situation
-        industry_scores = {
-            "Technology": 0.15,
-            "Finance": 0.12,
-            "Healthcare": 0.12,
-            "Energy": 0.1,
-            "Real Estate": 0.1,
-            "Communications": 0.08,
-            "Consumer Discretionary": 0.08,
-            "Industrials": 0.08,
-            "Thematic": 0.07,
-            "Utilities": 0.1
-        }
-        
-        return industry_scores.get(industry_focus, 0)
-
 def evaluate_etfs(etf_objects):
     # Calculate score for each ETF
-    scores = [etf.calculate_score() for etf in etf_objects]
+    scores = [etf.calculate_aggressive_score() for etf in etf_objects]
 
     # return scores
     return scores
